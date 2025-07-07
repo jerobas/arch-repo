2f1144f7ed7c89360f7d201aa0b70aa1e649b182
João Vitor Cezario Sanches <joaovitormanolo@gmail.com>
2025-07-07 17:43:47 -0300

    fix: move some part of setup.sh to build.yml

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
-pacman -Syu --noconfirm
-pacman -S --noconfirm base-devel
 useradd -m builduser
 echo 'builduser ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
 chown -R builduser:builduser .
